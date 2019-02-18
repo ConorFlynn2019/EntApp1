0ca330f3c09ce1d1cc4e865c5337e68fee14f006
Conor Flynn <S00191045@mail.itsligo.ie>
2019-02-18 13:18:01 +0000

    Completed Week Two

---
+++ WeekTwo/WeekTwo/Pages/taghelpplay.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WeekTwo.Pages
+{
+    public class taghelpplayModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        [BindProperty]
+        public int Number1 { get; set; }
+
+        [BindProperty]
+        [Display(Name = "Second Number")]
+        public int? Number2 { get; set; }
+
+        [BindProperty]
+        public Double Double1 { get; set; }
+
+        [BindProperty]
+        [Display(Name = "Second Double")]
+        public Double? Double2 { get; set; }
+
+        [BindProperty]
+        public Decimal Decimal1 { get; set; }
+
+        [BindProperty]
+        [Display(Name = "Second Decimal")]
+        public Decimal? Decimal2 { get; set; }
+
+        [BindProperty]
+        public float Float1 { get; set; }
+
+        [BindProperty]
+        [Display(Name = "Second Float")]
+        public float? Float2 { get; set; }
+
+        [BindProperty]
+        public DateTime Datet1 { get; set; }
+    }
+}
