070683ff8963eef34bb27291c5505870177cc22b
Conor Flynn <S00191045@mail.itsligo.ie>
2019-02-22 12:27:57 +0000

    Week 4

---
+++ EntAppSecond-master/EntAppSecond/Pages/FormWS.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EntAppSecond.Pages
+{
+    public class FormWSModel : PageModel
+    {
+
+        [BindProperty]
+        public string ProductCode { get; set; }
+
+        [BindProperty]
+        public bool[] Colour { get; set; } = new bool[3];
+
+
+        public void OnGet()
+        {
+        }
+    }
+
+
+
+
+}
