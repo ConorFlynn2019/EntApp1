b69ed8c96a04f7e04fbb013d91de7b3cb61c785c
Conor Flynn <S00191045@mail.itsligo.ie>
2019-02-26 11:35:44 +0000

    Validation One Complete
    First worksheet done

---
+++ EntAppSecond-master/EntAppSecond/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace EntAppSecond.Pages.Models
+{
+    public class Student
+    {
+        public Student()
+        {
+        }
+
+        [Required (ErrorMessage = "Please enter a Student ID")]
+        [Display(Name = "Student ID")]
+
+        public string StudentID { get; set; } = "";
+
+        [Required(ErrorMessage = "Please enter your first name")]
+        [Display(Name = "First Name")]
+
+        public string FirstName { get; set; } = "";
+
+        [Required(ErrorMessage = "Please enter your last name")]
+        [Display(Name = "Last Name")]
+
+        public string LastName { get; set; } = "";
+    }
+}
